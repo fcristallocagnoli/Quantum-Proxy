9a280d0ce0c2c89ff6451cfe97a2553854cc5f12
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-04-22 01:12:56 +0200

    update(utils): more date utilities

--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -1,9 +1,15 @@
 from datetime import datetime
+from passlib.context import CryptContext
+
+# region Miscelaneous
 
 
 def norm_id(db_document: dict) -> str:
     """Extracts the id from a MongoDB document and returns it as a string."""
     return str(db_document["_id"])
+
+
+# region Date related
 
 
 def convert_from_ms(millisec: int) -> str:
@@ -37,11 +43,38 @@ def convert_from_ms(millisec: int) -> str:
 def from_seconds_to_date(seconds) -> str:
     """Convierte y formatea a fecha a partir de tiempo en segundos"""
     date: datetime = datetime.fromtimestamp(seconds)
-    return date.strftime("%Y-%m-%d %H:%M:%S GMT%z")
+    return format_date(date)
 
 
 def get_current_time() -> str:
     """
     Devuelve la fecha actual con formato ISO-8601
     """
-    return datetime.now().strftime("%Y-%m-%d %H:%M:%S GMT%z")
+    return format_date(datetime.now())
+
+
+def fromisoformat(date: str) -> str:
+    """
+    Convierte un string con formato ISO-8601 a un objeto datetime
+    """
+    return format_date(datetime.fromisoformat(date))
+
+
+def format_date(date: datetime) -> str:
+    """
+    Formatea una fecha a un string con el formato ISO-8601
+    """
+    return date.strftime("%Y-%m-%d %H:%M:%S GMT%z")
+
+
+# region Password hashing
+
+pwd_context = CryptContext(schemes=["bcrypt", "unix_disabled"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def hash_password(password):
+    return pwd_context.hash(password)
