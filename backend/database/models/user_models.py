de15488dedd35cc0755e649e19dee8fda900b65c
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-04-21 21:39:50 +0200

    update(db): reorganize UserModel
    - Email and Password fields set to optional
    - Check whenever required in router

--- backend/database/models/user_models.py
+++ backend/database/models/user_models.py
@@ -1,12 +1,12 @@
 import datetime
 from typing import Annotated, Optional
-from bson import ObjectId
 from pydantic import BaseModel, BeforeValidator, ConfigDict, Field
 
 
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
 Date = Annotated[datetime.datetime, BeforeValidator(str)]
+
 
 class UserModel(BaseModel):
     # Así, el campo id se valida como "_id" y se selializa como "id"
@@ -15,8 +15,8 @@ class UserModel(BaseModel):
     )
     username: Optional[str] = Field(default=None)
     full_name: Optional[str] = Field(default=None)
-    email: str = Field(...)
-    hashed_password: str = Field(...)
+    email: str = Field(default=None)
+    password: str = Field(default=None)
     roles: Optional[list[str]] = Field(default=[])
     api_keys: Optional[dict[str, str]] = Field(default=None)
     created_at: Optional[Date] = Field(default=None)
@@ -28,27 +28,3 @@ class UserModel(BaseModel):
         # es decir, que no se validen, solo chequea que sea del tipo correcto
         arbitrary_types_allowed=True,
     )
-
-
-class UpdateUserModel(BaseModel):
-    # Así, el campo id se valida como "_id" y se selializa como "id"
-    id: Optional[PyObjectId] = Field(
-        validation_alias="_id", serialization_alias="id", default=None
-    )
-    username: Optional[str] = None
-    full_name: Optional[str] = Field(default=None)
-    email: Optional[str] = None
-    hashed_password: Optional[str] = None
-    roles: Optional[list[str]] = Field(default=[])
-    api_keys: Optional[dict[str, str]] = None
-    created_at: Optional[Date] = None
-    disabled: Optional[bool] = Field(default=False)
-    model_config = ConfigDict(
-        # Para poder incluir clases que no hereden de BaseModel como campos
-        # es decir, que no se validen, solo chequea que sea del tipo correcto
-        arbitrary_types_allowed=True,
-        json_encoders={ObjectId: str},
-    )
-
-
-UserDTO = type("UserDTO", (UpdateUserModel,), {})
