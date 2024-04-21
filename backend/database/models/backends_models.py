73b03e9305f64c803fb8219ff6072bc0764632ea
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-04-22 01:19:00 +0200

    feat(db): add backends' models

---
+++ backend/database/models/backends_models.py
@@ -0,0 +1,79 @@
+from typing import Any, Optional
+from pydantic import BaseModel, ConfigDict
+
+
+class ProviderFK(BaseModel):
+    id: str
+    name: str
+
+
+class BaseBackendModel(BaseModel):
+    id: str = None
+    provider: ProviderFK
+    backend_name: str
+    extra: Optional[list[str]] = None
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True,
+    )
+
+
+# --------------------------------------
+
+
+class Queue:
+    def __init__(self, queue_type: str, value: Any):
+        self.type = queue_type
+        self.value = value
+
+    def __str__(self) -> str:
+        return f"Queue({self.value}) = {self.type}"
+
+
+class IonQBackend(BaseBackendModel):
+    status: str
+    qubits: int
+    queue: Queue
+    last_updated: str
+    # may be processing jobs slower than usual
+    degraded: bool
+    # solo aparece usando token
+    has_access: Optional[bool] = None
+    # para el hardware real
+    characterization: Optional[str] = None
+    # Para los simuladores
+    noise_models: Optional[list[str]] = None
+    # campos que incluye IonQ
+    extra: list[str]
+
+
+# --------------------------------------
+
+
+class IBMBackend(BaseBackendModel):
+    status: str
+    qubits: int
+    queue: Queue
+    last_updated: str
+    basis_gates: list[str]
+    clops_h: int
+    credits_required: bool
+    max_experiments: int
+    max_shots: int
+    extra: list[str]
+
+
+# --------------------------------------
+
+
+class RigettiBackend(BaseBackendModel):
+    qubits: int
+    rep_rate: str
+    median_t1: str
+    median_t2: str
+    median_sim_1q_fidelity: str
+    median_2q_xy_fidelity: str
+    median_2q_cz_fidelity: str
+    median_ro_fidelity: str
+    median_active_reset_fidelity: str
+    extra: list[str]
