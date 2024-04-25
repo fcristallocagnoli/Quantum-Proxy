a4852e7eb2f8d152ecf4494e264e584d66a74e27
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-04-26 00:59:28 +0200

    update(wscrap): update scraper module

--- backend/modules/scraper_module.py
+++ backend/modules/scraper_module.py
@@ -1,7 +1,7 @@
 import ast
 import importlib.util
-import os
 from contextlib import contextmanager
+from pathlib import Path
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -10,9 +10,6 @@ from database.models.providers_models import BaseProviderModel
 from database.mongo_client import db_find_provider
 from utils.utils import norm_id
 from utils.email_utils import error_mail
-
-SCRAPER_FILE = "code_to_compile.py"
-SCRAPER_MODULE = SCRAPER_FILE.split(".")[0]
 
 
 @contextmanager
@@ -35,19 +32,6 @@ def innit_driver(url: str):
         print("Webdriver cerrado.")
 
 
-@contextmanager
-def temp_file_manager(code: str):
-    """Context manager para escribir y leer un archivo temporal."""
-    try:
-        with open(SCRAPER_FILE, "w") as f:
-            f.write(code)
-        yield
-    finally:
-        print("Borrando archivo temporal...")
-        os.remove(SCRAPER_FILE)
-        print("Archivo temporal borrado.")
-
-
 def parse_code(code: str):
     """
     Validar el código antes de ejecutarlo.
@@ -65,6 +49,31 @@ def parse_code(code: str):
                 )
 
 
+def check_code(file_path: Path):
+    """
+    Parse and check the code before executing it.
+    - Avoid calls to dangerous functions like eval() or exec().
+    """
+    if not file_path.exists():
+        print(f"File {file_path.name} not found.")
+        return False
+    with open(file_path, encoding="utf-8") as f:
+        code = f.read()
+    try:
+        parse_code(code)
+    except Exception as e:
+        match error := type(e).__name__:
+            case ValueError.__name__:
+                print(f"{error}: Error de sintaxis en el código\n", e)
+            case SyntaxError.__name__:
+                print(f"{error}: Error al validar el código\n", e)
+            case _:
+                print(f"{error}: Error al ejecutar el código\n", e)
+                # raise type("ParseException", (Exception,), {"msg": error})
+        return False
+    return True
+
+
 def fetch_from_ws(provider: BaseProviderModel) -> list[dict]:
     """
     Fetch data from a website using web scraping.
@@ -74,53 +83,39 @@ def fetch_from_ws(provider: BaseProviderModel) -> list[dict]:
     """
     print("Fetching from web scraping...")
     func = provider.backend_request.scraper.func_to_eval
-    code = provider.backend_request.scraper.code_to_compile
-    try:
-        parse_code(code)
-    except Exception as e:
-        match type(e).__name__:
-            case ValueError.__name__:
-                error = f"{error}: Error de sintaxis en el código"
-            case SyntaxError.__name__:
-                error = f"{error}: Error al validar el código"
-            case _:
-                error = f"{error}: Error al ejecutar el código"
-        print(error)
-        # raise type("ParseException", (Exception,), {"msg": error})
+    file_name = provider.backend_request.scraper.scraper_file_name
+    file_path = (
+        Path(__file__).parent.joinpath("source_files").joinpath(f"{file_name}.py")
+    )
+
+    if not check_code(file_path):
         return []
 
-    with temp_file_manager(code):
-        # Ejecutar el código en un contexto que proporciona un webdriver
-        with innit_driver(provider.backend_request.base_url) as driver:
-            # Cargar el módulo desde el archivo externo
-            especificacion = importlib.util.spec_from_file_location(
-                SCRAPER_MODULE, SCRAPER_FILE
-            )
-            modulo = importlib.util.module_from_spec(especificacion)
-            especificacion.loader.exec_module(modulo)
+    # Ejecutar el código en un contexto que proporciona un webdriver
+    with innit_driver(provider.backend_request.base_url) as driver:
+        # Cargar el módulo desde el archivo externo
+        especificacion = importlib.util.spec_from_file_location(file_name, file_path)
+        modulo = importlib.util.module_from_spec(especificacion)
+        especificacion.loader.exec_module(modulo)
 
-            # Llamar a la función del fragmento de código externo
-            raw_output: list[dict] = []
-            try:
-                raw_output = getattr(modulo, func)(driver)
-            except NoSuchElementException as error:
-                print(f"Error al obtener los datos: {error.msg}")
-                error_mail(error, "Error al obtener los datos via webscraping.")
-            except Exception as error:
-                print(f"Error inesperado: {error}")
-                error_mail(
-                    error, "Error inesperado al obtener los datos via webscraping."
-                )
-            provider_data = {
-                "provider_id": norm_id(
-                    db_find_provider(filter={"name": provider.name})
-                ),
-                "provider_name": provider.name,
-            }
-            raw_output = list(
-                map(
-                    lambda back: back.update({"provider": provider_data}) or back,
-                    raw_output,
-                )
+        # Llamar a la función del fragmento de código externo
+        raw_output: list[dict] = []
+        try:
+            raw_output = getattr(modulo, func)(driver)
+        except NoSuchElementException as error:
+            print(f"Error al obtener los datos: {error.msg}")
+            error_mail(error, "Error al obtener los datos via webscraping.")
+        except Exception as error:
+            print(f"Error inesperado: {error}")
+            error_mail(error, "Error inesperado al obtener los datos via webscraping.")
+        provider_data = {
+            "provider_id": norm_id(db_find_provider(filter={"name": provider.name})),
+            "provider_name": provider.name,
+        }
+        raw_output = list(
+            map(
+                lambda back: back.update({"provider": provider_data}) or back,
+                raw_output,
             )
+        )
     return raw_output
