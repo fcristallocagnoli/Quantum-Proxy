5fa8968c923fafe53d17dc426b347dcd2ea1d293
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-05-13 00:58:25 +0200

    feat(utils): add scheduler functions module
    - Add a new module `scheduler_functions.py` to handle job scheduling and backend refreshing.
    - Implement the `job_manager` function to handle job execution and logging.
    - Implement the `init_providers` function to initialize providers in the database.
    - Implement the `refresh_backend` function to refresh the backends of a provider.
    - Implement the `init_backends` function to initialize backends using a scheduler.
    
    Note: This commit adds new functionality related to job scheduling and backend refreshing.

---
+++ backend/database/provider_data.py
@@ -0,0 +1,128 @@
+from braket.aws import AwsDevice
+from dotenv import load_dotenv
+
+from utils.utils import norm_str
+
+load_dotenv()
+
+providers_api_data = [
+    {
+        "name": "IonQ",
+        "pid": "native.ionq",
+        "description": None,
+        "url": "https://ionq.com/",
+        "from_third_party": False,
+        "backend_request": {
+            "fetch_method": "API",
+            "base_url": "http://api.ionq.co/v0.3",
+            "auth": {
+                "Authorization": "apiKey TOKEN",
+            },
+            "third_party": None,
+            "module": {
+                "func_to_eval": "get_backends",
+                "module_file": "ionq_api_code",
+            },
+        },
+        "backends_ids": [],
+    },
+    {
+        "name": "IBM Quantum",
+        "pid": "native.ibm_quantum",
+        "description": None,
+        "url": "https://www.ibm.com/quantum",
+        "from_third_party": False,
+        "backend_request": {
+            "fetch_method": "API",
+            "base_url": "https://api.quantum-computing.ibm.com/runtime",
+            "auth": {
+                "Authorization": "Bearer TOKEN",
+            },
+            "module": {
+                "func_to_eval": "get_backends",
+                "module_file": "ibm_api_code",
+            },
+        },
+        "backends_ids": [],
+    },
+]
+
+providers_ws_data = [
+    {
+        "name": "Rigetti",
+        "pid": "native.rigetti",
+        "description": None,
+        "url": "https://www.rigetti.com/",
+        "from_third_party": False,
+        "backend_request": {
+            "fetch_method": "WEB-SCRAPING",
+            "base_url": "https://qcs.rigetti.com/qpus",
+            "module": {
+                "func_to_eval": "get_backends",
+                "module_file": "rigetti_ws_code_v2",
+            },
+        },
+        "backends_ids": [],
+    },
+]
+
+providers_sdk_data = [
+    {
+        "name": "Amazon Braket",
+        "pid": "native.amazon_braket",
+        "description": None,
+        "url": "https://aws.amazon.com/braket/",
+        "from_third_party": False,
+        "backend_request": {
+            "fetch_method": "SDK",
+            "auth": {
+                "env_vars": [
+                    "AWS_ACCESS_KEY_ID",
+                    "AWS_SECRET_ACCESS_KEY",
+                    "AWS_REGION",
+                ]
+            },
+            "module": {
+                "func_to_eval": "get_backends",
+                "module_file": "braket_sdk_code",
+            },
+        },
+        "backends_ids": [],
+    },
+]
+
+
+def get_braket_providers():
+    output = []
+    devices = AwsDevice.get_devices()
+    providers = []
+    for device in devices:
+        if device.provider_name in providers:
+            continue
+        providers.append(device.provider_name)
+        template = {
+            "name": device.provider_name,
+            "pid": f"amazon_braket.{norm_str(device.provider_name)}",
+            "description": None,
+            "url": None,
+            "from_third_party": True,
+            "third_party": {
+                "third_party_id": None,
+                "third_party_name": "Amazon Braket",
+            },
+            "backends_ids": [],
+        }
+        output.append(template)
+    return output
+
+
+# O se añaden a mano, o se puede recurrrir a la función anterior
+# Nota: se necesita cargar el archivo .env con las credenciales de AWS
+providers_data_from_braket = get_braket_providers()
+
+providers_data = (
+    providers_api_data
+    + providers_ws_data
+    + providers_sdk_data
+    + providers_data_from_braket
+)
---
+++ backend/utils/scheduler_functions.py
@@ -0,0 +1,62 @@
+from bson import ObjectId
+from fastapi.logger import logger
+from database.models.providers_models import BaseProviderModel, ThirdPartyEnum
+from modules.gateway_module import fetch_data
+from database.provider_data import providers_data
+from database.mongo_client import (
+    db_delete_backends,
+    db_find_provider,
+    db_find_providers,
+    db_insert_backends,
+    db_insert_providers,
+    db_update_provider,
+    db_update_providers,
+)
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def job_manager(event):
+    if event.exception:
+        logger.error(f"Job {event.job_id} failed with exception {event.exception}")
+        return
+    logger.debug(f"Job {event.job_id} executed successfully")
+
+
+def init_providers():
+    db_insert_providers(providers_data)
+
+    for third_party in [e.value for e in ThirdPartyEnum]:
+        formated_tp = third_party.lower().replace(" ", "_")
+        provider = db_find_provider(filter={"pid": f"native.{formated_tp}"})
+        db_update_providers(
+            filter={"third_party.third_party_name": third_party},
+            cambios={"$set": {"third_party.third_party_id": provider["_id"]}},
+        )
+
+
+def refresh_backend(provider: BaseProviderModel):
+    """
+    Refreshes the backends of a provider
+
+    Args:
+    - provider (BaseProviderModel): The provider to refresh
+        - id (str): The provider's database id
+    """
+    old_ids = list(map(lambda id: ObjectId(id), provider.backends_ids))
+    db_delete_backends(filter={"_id": {"$in": old_ids}})
+
+    datos = fetch_data(provider)
+    backends_ids = db_insert_backends(datos)
+
+    db_update_provider(
+        filter={"_id": ObjectId(provider.id)},
+        cambios={"$set": {"backends_ids": backends_ids}},
+    )
+
+
+def init_backends(scheduler: BackgroundScheduler):
+    providers = db_find_providers(filter={"from_third_party": False})
+    providers = list(map(lambda p: BaseProviderModel(**p), providers))
+    for provider in providers:
+        logger.info(f"Processing provider: {provider.name} with id: {provider.id}...")
+        scheduler.add_job(func=refresh_backend, args=[provider], id=provider.name)
