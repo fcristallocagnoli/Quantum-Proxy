30a804a8e83ed46246d7f24256d2ae0616bbcfa2
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-05-13 01:05:40 +0200

    feat(main): add cheduler setup
    The code changes logger initialization and scheduler setup to the FastAPI application. This allows for better logging of events and the scheduling of background jobs for backend initialization and refreshing.

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,78 @@
+import logging
+import sys
+from contextlib import asynccontextmanager
+
+from apscheduler.events import EVENT_JOB_ERROR, EVENT_JOB_EXECUTED
+from apscheduler.schedulers.background import BackgroundScheduler
+from database.mongo_client import is_empty
 from fastapi import FastAPI
-from routers import provider_router, user_router, auth_router
+from fastapi.logger import logger
+from routers import auth_router, provider_router, user_router
+from utils.scheduler_functions import init_backends, init_providers, job_manager
 
 
-app = FastAPI(
-    title="Quantum Proxy API",
-)
+def init_logger():
+    logger.setLevel(logging.DEBUG)
+    stream_handler = logging.StreamHandler(sys.stdout)
+    log_formatter = logging.Formatter(
+        "%(asctime)s [%(processName)s: %(process)d] [%(threadName)s: %(thread)d] [%(levelname)s] %(name)s: %(message)s"
+    )
+    stream_handler.setFormatter(log_formatter)
+    logger.addHandler(stream_handler)
+
+
+def init_database(scheduler: BackgroundScheduler):
+    # Si la coleccion de proveedores esta vacia
+    if is_empty("providers"):
+        logger.debug("Initializing providers")
+        init_providers()
+    # Si la coleccion de backends esta vacia
+    if is_empty("backends"):
+        logger.debug("Calling init_backends function (in first run)")
+        scheduler.add_job(
+            func=init_backends,
+            args=[scheduler],
+            id="job_init_backends",
+            replace_existing=True,
+        )
+    # Si la coleccion de backends NO esta vacia
+    else:
+        logger.debug("Calling init_backends function (scheduled) every day at 2:00 AM")
+        scheduler.add_job(
+            func=init_backends,
+            args=[scheduler],
+            trigger="cron",
+            minute="0",
+            hour="2",
+            id="job_init_backends",
+            replace_existing=True,
+        )
+
+
+def init_scheduler():
+    scheduler = BackgroundScheduler()
+    scheduler.add_listener(job_manager, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
+    return scheduler
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Inicializar el logger
+    init_logger()
+    # ---------------------
+    # Inicializar el scheduler
+    scheduler = init_scheduler()
+    init_database(scheduler)
+    # ---------------------
+    logger.debug("Starting scheduler")
+    logger.debug(f"Jobs queued: {scheduler.get_jobs()}")
+    scheduler.start()
+    yield
+    scheduler.shutdown()
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 app.include_router(
     auth_router.router,
