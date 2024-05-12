0f7b81a2affabeaa53ca3cda70051f65acc63614
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-05-13 01:25:14 +0200

    update(db): update providers' models

--- backend/database/models/providers_models.py
+++ backend/database/models/providers_models.py
@@ -3,11 +3,23 @@ from typing import Annotated, Any, Literal, Optional
 
 from pydantic import BaseModel, BeforeValidator, ConfigDict, Field
 
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
 
 class ProviderName(StrEnum):
     IONQ = "IonQ"
     IBM = "IBM Quantum"
     RIGETTI = "Rigetti"
+
+
+class ThirdPartyEnum(StrEnum):
+    AWS = "Amazon Braket"
+    AZURE = "Azure Quantum"
+
+
+class ThirdPartyKey(BaseModel):
+    third_party_id: Optional[PyObjectId] = None
+    third_party_name: ThirdPartyEnum
 
 
 class Module(BaseModel):
@@ -19,18 +31,25 @@ class Module(BaseModel):
 class ScraperRequest(BaseModel):
     fetch_method: Literal["WEB-SCRAPING"] = "WEB-SCRAPING"
     base_url: str
+    # auth: Optional[dict[str, Any]] = None
     module: Module
 
 
 class APIRequest(BaseModel):
     fetch_method: Literal["API"] = "API"
     base_url: str
+    auth: Optional[dict[str, str]] = None
     module: Module
-    headers: Optional[dict[str, str]] = None
-    params: Optional[dict[str, str]] = None
 
 
-PyObjectId = Annotated[str, BeforeValidator(str)]
+class SDKRequest(BaseModel):
+    fetch_method: Literal["SDK"] = "SDK"
+    auth: Optional[dict[str, list]] = None
+    third_party: Optional[ThirdPartyEnum] = None
+    module: Module
+
+
+RequestTypes = APIRequest | SDKRequest | ScraperRequest
 
 
 class BaseProviderModel(BaseModel):
@@ -46,19 +65,20 @@ class BaseProviderModel(BaseModel):
         validation_alias="_id", serialization_alias="id", default=None
     )
     name: Optional[str] = Field(default=None)
-    description: Any = Field(default=None)
+    pid: Optional[str] = Field(default=None)
+    description: Optional[Any] = Field(default=None)
     url: Optional[str] = Field(default=None)
+    from_third_party: Optional[bool] = Field(default=None)
+    third_party: Optional[ThirdPartyKey] = Field(default=None)
 
-    backend_request: Optional[APIRequest | ScraperRequest] = Field(
+    backend_request: Optional[RequestTypes] = Field(
         default=None,
         discriminator="fetch_method",
     )
 
-    backends_ids: Optional[list[str]] = Field(default=None)
+    backends_ids: Optional[list[PyObjectId]] = Field(default=None)
 
-    last_checked: Optional[str] = None
-    created_at: Optional[str] = None
-    updated_at: Optional[str] = None
+    last_updated_at: Optional[str] = None
     model_config = ConfigDict(
         # Para permitir "Class(id=...)", en vez de "Class(_id=...)"
         populate_by_name=True,
