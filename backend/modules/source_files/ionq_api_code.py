61fa7809d9ca97b41f724edc7ce255a78d2df14e
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-05-12 23:38:10 +0200

    feat(source_files): update IonQ API code
    - Refactor the `ionq_api_code.py` module to improve backend processing.
    - Add a new function `process_backend` to handle backend characterization.
    - Update the `get_backends` function to use the `provider_key` parameter instead of `provider_code` for authentication.
    - Remove unnecessary code related to database operations.
    - Update the code to conform to the new model.

--- backend/modules/source_files/ionq_api_code.py
+++ backend/modules/source_files/ionq_api_code.py
@@ -1,15 +1,29 @@
 from typing import Any
 
 import requests
-from database.mongo_client import db_find_provider
 from modules.api_module import APIRequest, get_auth_if_needed
-from utils.utils import norm_id
+
+
+def process_backend(backend: dict) -> dict:
+    backend_name = backend["backend"]
+    # Si no es un simulador, obtenemos su caracterización
+    if backend_name == "simulator":
+        return backend
+    # Eliminamos el antiguo enlace
+    del backend["characterization_url"]
+    # Recuperamos la caracterización
+    charact: dict = requests.get(
+        f"{backend['characterization_url']}",
+        headers={"Authorization": backend["auth"]},
+    ).json()
+    # No me interesa la conectividad
+    charact.pop("connectivity", None)
 
 
 def get_backends(request: APIRequest) -> list[dict[str, Any]]:
 
     base_url = request.base_url
-    auth = get_auth_if_needed(request, provider_code="ionq")
+    auth = get_auth_if_needed(request, provider_key="ionq")
     # Obtenemos los backends
     backends = requests.get(
         f"{base_url}/backends",
@@ -17,15 +31,10 @@ def get_backends(request: APIRequest) -> list[dict[str, Any]]:
         headers={"Authorization": auth},
     ).json()
 
-    # Iteramos sobre los backends para obtener la ultima caracterización
-    # de cada uno y guardarla en la base de datos
     for idx, backend in enumerate(backends):
         backend_name = backend["backend"]
+        backend["provider"] = None
         # Si no es un simulador, obtenemos su caracterización
-        backend["provider"] = {
-            "provider_id": norm_id(db_find_provider(filter={"name": "IonQ"})),
-            "provider_name": "IonQ",
-        }
         if backend_name == "simulator":
             continue
         # Eliminamos el antiguo enlace
@@ -39,21 +48,9 @@ def get_backends(request: APIRequest) -> list[dict[str, Any]]:
         charact.pop("connectivity", None)
         # TODO: Formatear la fecha a un formato generico
         charact["date"] = charact["date"]
-        # Insertamos la caracterización en la base de datos
-        # y guardamos el ObjectId en el campo extra como referencia
+        # Insertamos la caracterización
         backends[idx]["extra"] = {
-            # "characterization": insert_characterization(
-            #     f"{self.name.lower()}_{backend_name.lower()}", charact
-            # ).inserted_id
-            # "characterization_id": f"{"IonQ".lower()}_{backend_name.lower()}",
             "characterization_id": f"ionq.{charact["backend"]}",
             "characterization": charact
         }
-    # Guardamos los backends en la base de datos
-    # provider: dict = {
-    #     "id": str(find_provider_by_name(ProviderName.IONQ)["_id"]),
-    #     "name": ProviderName.IONQ,
-    # }
-    # for backend in backends:
-    #     insert_backend({"provider": provider, **backend})
     return backends
