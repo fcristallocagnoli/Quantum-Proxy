b1bfc927169c00429ba6cd1f6b0ca4595eba4559
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-05-15 00:55:01 +0200

    refactor(main): remove references from jwt-auth
    - The auth module will be implemented in the future.
    - This commit removes the import statement and the inclusion of the router in the FastAPI application.

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from database.mongo_client import is_empty
 from fastapi import FastAPI
 from fastapi.logger import logger
-from routers import auth_router, provider_router, user_router
+from routers import provider_router, user_router
 from utils.scheduler_functions import init_backends, init_providers, job_manager
 
 
@@ -28,7 +28,7 @@ def init_database(scheduler: BackgroundScheduler):
         init_providers()
     # Si la coleccion de backends esta vacia
     if is_empty("backends"):
-        logger.debug("Calling init_backends function (in first run)")
+        logger.debug("Setting 'init_backends' function to run immediately")
         scheduler.add_job(
             func=init_backends,
             args=[scheduler],
@@ -37,7 +37,7 @@ def init_database(scheduler: BackgroundScheduler):
         )
     # Si la coleccion de backends NO esta vacia
     else:
-        logger.debug("Calling init_backends function (scheduled) every day at 2:00 AM")
+        logger.debug("Setting 'init_backends' function to run every day at 2:00 AM")
         scheduler.add_job(
             func=init_backends,
             args=[scheduler],
@@ -73,10 +73,6 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-
-app.include_router(
-    auth_router.router,
-)
 
 app.include_router(
     user_router.router,
