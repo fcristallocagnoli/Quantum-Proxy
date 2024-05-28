f76277b56050c231d31d2f361510d21ebdda7fd0
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-05-28 21:02:28 +0200

    refactor(frontend): update auth.guard.ts
    - Interface 'CanActivate' is deprecated
    - Change `AuthGuard` to use a function instead of a class
    - Update references to `AuthGuard` in `app-routing.module.ts` and `app.routes.ts` to use the new function name `authGuard`

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home';
-import { AuthGuard } from './_helpers';
+import { authGuard } from './_helpers';
 import { Role } from './_models';
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
@@ -10,10 +10,10 @@ const adminModule = () => import('./admin/admin.module').then(x => x.AdminModule
 const profileModule = () => import('./profile/profile.module').then(x => x.ProfileModule);
 
 const routes: Routes = [
-    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: '', component: HomeComponent, canActivate: [authGuard] },
     { path: 'account', loadChildren: accountModule },
-    { path: 'profile', loadChildren: profileModule, canActivate: [AuthGuard] },
-    { path: 'admin', loadChildren: adminModule, canActivate: [AuthGuard], data: { roles: [Role.Admin] } },
+    { path: 'profile', loadChildren: profileModule, canActivate: [authGuard] },
+    { path: 'admin', loadChildren: adminModule, canActivate: [authGuard], data: { roles: [Role.Admin] } },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,13 +2,13 @@
 
 import { Routes } from '@angular/router';
 import { AlertComponent } from './_components/alert.component';
-import { AuthGuard } from './_helpers/auth.guard';
+import { authGuard } from './_helpers/auth.guard';
 import { Role } from './_models';
 
 export const routes: Routes = [
-  { path: '', component: AlertComponent, canActivate: [AuthGuard] },
+  { path: '', component: AlertComponent, canActivate: [authGuard] },
   { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), canActivate: [AuthGuard], data: { roles: [Role.Admin] } },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), canActivate: [authGuard], data: { roles: [Role.Admin] } },
 
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
