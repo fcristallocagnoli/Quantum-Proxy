61716a1f5da3247b431f362bf15ad3d3e7b95ed3
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-06-05 01:58:10 +0200

    feat(frontend): improve UI
    - Adjust column widths in the account list table for better readability
    - Add scroll-to-top functionality in the app component for better user experience
    - Update styles to prevent nav overlap in the app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 import { AccountService } from './_services';
 import { Account, Role } from './_models';
@@ -11,6 +11,8 @@ export class AppComponent {
   Role = Role;
   account?: Account | null;
 
+  showScroll: boolean = false;
+
   constructor(private accountService: AccountService) {
     this.accountService.account.subscribe(x => this.account = x);
   }
@@ -19,5 +21,19 @@ export class AppComponent {
     this.accountService.logout();
     // para solucionar un pequeño bug. el dropdown no se cierra al hacer logout
     document.getElementById('logout-user')?.click();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop;
+    this.showScroll = scrollPosition > 300;
+  }
+
+  scrollToTop(): void {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
   }
 }
