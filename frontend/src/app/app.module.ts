0fa4d48edeb2fae72e06f4a64dd95ed43b35ba2c
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-06-05 05:13:33 +0200

    feat(frontend): remove fake backend
    - All features have been implemented

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-// used to create fake backend
-import { fakeBackendProvider } from './_helpers';
-
 import { AppRoutingModule } from './app-routing.module';
 import { JwtInterceptor, ErrorInterceptor, appInitializer } from './_helpers';
 import { AccountService } from './_services';
@@ -25,8 +22,6 @@ import { HashPipe } from "./_helpers/hash.pipe";
         { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [AccountService] },
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-        // provider used to create fake backend
-        fakeBackendProvider
     ],
     bootstrap: [AppComponent],
     imports: [
