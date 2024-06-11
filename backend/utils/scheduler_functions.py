ab21fda55873b53160f91a303281cf9b731f7877
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-06-11 22:03:46 +0200

    fix(scheduler): update refresh_backends func.

--- backend/utils/scheduler_functions.py
+++ backend/utils/scheduler_functions.py
@@ -1,10 +1,12 @@
 from bson import ObjectId
 from fastapi.logger import logger
+from utils.utils import get_current_datetime
 from database.models.providers_models import BaseProviderModel, ThirdPartyEnum
 from modules.gateway_module import fetch_data
 from database.provider_data import providers_data
 from database.mongo_client import (
     db_delete_backends,
+    db_find_backends,
     db_find_provider,
     db_find_providers,
     db_insert_backends,
@@ -42,15 +44,46 @@ def refresh_backends(provider: BaseProviderModel):
     - provider (BaseProviderModel): The provider to refresh
         - id (str): The provider's database id
     """
+    # Borramos los backends antiguos
     old_ids = list(map(lambda id: ObjectId(id), provider.backends_ids))
     db_delete_backends(filter={"_id": {"$in": old_ids}})
 
+    # Borramos las referencias a los backends antiguos
+    db_update_providers(
+        filter={"backends_ids": {"$in": old_ids}},
+        cambios={"$set": {"backends_ids": []}},
+    )
+
+    # Obtenemos los nuevos backends
     datos = fetch_data(provider)
     backends_ids = db_insert_backends(datos)
 
+    # Si el proveedor es de terceros, hay que actualizar los proveedores que ofrece
+    if provider.pid.split(".")[1] in [e.value.lower() for e in ThirdPartyEnum]:
+
+        # Obtenemos los backends asociados
+        for backend in db_find_backends(filter={"_id": {"$in": backends_ids}}):
+            # Si el nombre coincide, y proviene de terceros, se añade el id del backend
+            db_update_provider(
+                filter={
+                    "name": backend["provider"]["provider_name"],
+                    "from_third_party": True,
+                },
+                cambios={
+                    "$push": {"backends_ids": backend["_id"]},
+                    "$set": {"last_checked": get_current_datetime()},
+                },
+            )
+
+    # Para el resto de proveedores, se actualiza la lista de ids de backends
     db_update_provider(
         filter={"_id": ObjectId(provider.id)},
-        cambios={"$set": {"backends_ids": backends_ids}},
+        cambios={
+            "$set": {
+                "backends_ids": backends_ids,
+                "last_checked": get_current_datetime(),
+            }
+        },
     )
 
 
