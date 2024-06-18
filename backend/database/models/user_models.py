48235a55f6b087fd8d677d3e78732be24b75a38a
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-06-18 20:16:06 +0200

    refactor(user_model): change api_keys field default value
    - Now initializes with an empty dictionary

--- backend/database/models/user_models.py
+++ backend/database/models/user_models.py
@@ -19,7 +19,7 @@ class UserModel(BaseModel):
     email: str = Field(default=None)
     password: str = Field(default=None)
     roles: Optional[list[str]] = Field(default=[])
-    api_keys: Optional[dict[str, dict]] = Field(default=None)
+    api_keys: Optional[dict[str, dict]] = Field(default={})
     created_at: Optional[Date] = Field(default=None)
     disabled: Optional[bool] = Field(default=False)
     model_config = ConfigDict(
