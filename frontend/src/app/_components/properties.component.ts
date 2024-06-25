f1f40d6873bf417b79c86d3f8be505bc8043890a
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-06-25 05:15:31 +0200

    feat(frontend): add more properties components
    - Add 'system-props'
    - Add modal for characterizations

--- frontend/src/app/_components/properties.component.ts
+++ frontend/src/app/_components/properties.component.ts
@@ -1,5 +1,8 @@
-import { TitleCasePipe } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { CommonModule, JsonPipe, TitleCasePipe } from '@angular/common';
+import { Component, Input, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { System } from '@app/_models';
+import { NgbActiveModal, NgbModal, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'status',
@@ -57,20 +60,397 @@ export class StatusComponent {
     template: `
         @switch (queue?.type) {
         @case ("jobs_remaining") {
-        Queued jobs:
+        @if(!isTable) {
+            Queued jobs:
+        }@else {
+            Queued jobs
+        }
         }
         @case ("avg_time") {
-        Queue time:
+        @if(!isTable) {
+            Queue time:
+        }@else {
+            Queue time
+        }
         }
         @default {
-        Queue:
+        @if(!isTable) {
+            Queue:
+        }@else {
+            Queue
+        }
         }
         }
     `
 })
 export class QueueComponent {
     @Input() queue?: { type: string, value: string };
+    @Input() isTable?: boolean = false;
 
     constructor() { }
 
 }
+
+@Component({
+    selector: 'system-props',
+    standalone: true,
+    imports: [CommonModule, StatusComponent, QueueComponent, RouterLink],
+    template: `
+        <div class="card">
+            <table class="table table-striped table-borderless mb-0">
+                <thead>
+                    <tr>
+                        <th style="width:20%;"></th>
+                        @for (system of systems; track $index) {
+                        <th style="width:30%;">
+                            <!-- Se podria añadir el logo, haria falta obtener pagina web del proveedor -->
+                            <!-- <img src="{{system.provider.website}}/favicon.ico" alt="Provider Logo" class="provider-logo"> -->
+                            <a routerLink="/systems/{{ system.bid }}" class="link-underline link-underline-opacity-0"
+                            style="color: inherit;">
+                                {{ normalizeName(system.backend_name) }}
+                                {{ (system.provider.provider_from ? ' (' + system.provider.provider_from + ')' : '') }}
+                            </a>
+                        </th>
+                    }
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- extra fields (in common) -->
+                    <tr>
+                        <td>Provider</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.provider.provider_name }}
+                            {{ system.provider.provider_from ? '(' + system.provider.provider_from + ')' : ''}}
+                        </td>
+                        }
+                    </tr>
+                    <tr>
+                        <td>Status</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            <status [status]="system.status"></status>
+                        </td>
+                        }
+                    </tr>
+                    <tr>
+                        <td>Qubits</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.qubits}}
+                        </td>
+                        }
+                    </tr>
+                    <tr>
+                        <td>Queue</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            <queue [queue]="system.queue"></queue>
+                            {{ system.queue?.value ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    <!-- extra fields (ionq) -->
+                    @if (anyIsDefined(systems, 'degraded')) {
+                    <tr>
+                        <td>Degraded</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.degraded ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    }
+                    @if (anyIsDefined(systems, 'has_access')) {
+                    <tr>
+                        <td>Has Access</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.has_access ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    }
+                    @if (anyIsDefined(systems, 'characterization')) {
+                    <tr>
+                        <td>Characterization</td>
+                        @for (system of systems; track $index) {
+                        @if (system.characterization) {
+                        <td>
+                            <button class="btn btn-primary btn-sm" (click)="openModal(system)">View
+                                Characterization</button>
+                        </td>
+                        }@else {
+                        <td>N/A</td>
+                        }
+                        }
+                    </tr>
+                    }
+                    <!-- extra fields (braket) -->
+                    @if (anyIsDefined(systems, 'gates_supported')) {
+                    <tr>
+                        <td>Gates Supported</td>
+                        @for (system of systems; track $index) {
+                        @if (system.gates_supported) {
+                        <td>{{ system.gates_supported.join(', ') }}</td>
+                        }@else {
+                        <td>N/A</td>
+                        }
+                        }
+                    </tr>
+                    }
+                    @if (anyIsDefined(systems, 'shots_range')) {
+                    <tr>
+                        <td>Shots Range</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.shots_range?.min ?? 'N/A' }} - {{ system.shots_range?.max ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    }
+                    @if (anyIsDefined(systems, 'device_cost')) {
+                    <tr>
+                        <td>Device Cost</td>
+                        @for (system of systems; track $index) {
+                        @if (system.device_cost) {
+                        <td>{{ system.device_cost.price | currency }}/{{ system.device_cost.unit }}</td>
+                        }@else {
+                        <td>N/A</td>
+                        }
+                        }
+                    </tr>
+                    }
+                    <!-- extra fields (rigetti) -->
+                    @if (anyIsDefined(systems, 'rep_rate')) {
+                    <tr>
+                        <td>Rep Rate</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.rep_rate ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    }
+                    @if (anyIsDefined(systems, 'median_t1')) {
+                    <tr>
+                        <td>Median T1</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.median_t1 ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    }
+                    @if (anyIsDefined(systems, 'median_t2')) {
+                    <tr>
+                        <td>Median T2</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.median_t2 ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    }
+                    @if (anyIsDefined(systems, 'median_sim_1q_fidelity')) {
+                    <tr>
+                        <td>Median Sim 1Q Fidelity</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.median_sim_1q_fidelity ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    }
+                    @if (anyIsDefined(systems, 'median_2q_xy_fidelity')) {
+                    <tr>
+                        <td>Median 2Q XY Fidelity</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.median_2q_xy_fidelity ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    }
+                    @if (anyIsDefined(systems, 'median_2q_cz_fidelity')) {
+                    <tr>
+                        <td>Median 2Q CZ Fidelity</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.median_2q_cz_fidelity ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    }
+                    @if (anyIsDefined(systems, 'median_ro_fidelity')) {
+                    <tr>
+                        <td>Median RO Fidelity</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.median_ro_fidelity ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    }
+                    @if (anyIsDefined(systems, 'median_active_reset_fidelity')) {
+                    <tr>
+                        <td>Median Active Reset Fidelity</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.median_active_reset_fidelity ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    }
+                    <!-- extra fields (ibm) -->
+                    @if (anyIsDefined(systems, 'basis_gates')) {
+                    <tr>
+                        <td>Basis Gates</td>
+                        @for (system of systems; track $index) {
+                        @if (system.basis_gates) {
+                        <td>{{ system.basis_gates.join(', ') }}</td>
+                        }@else {
+                        <td>N/A</td>
+                        }
+                        }
+                    </tr>
+                    }
+                    @if (anyIsDefined(systems, 'clops_h')) {
+                    <tr>
+                        <td>Clops H</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.clops_h ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    }
+                    @if (anyIsDefined(systems, 'credits_required')) {
+                    <tr>
+                        <td>Credits Required</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.credits_required ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    }
+                    @if (anyIsDefined(systems, 'max_experiments')) {
+                    <tr>
+                        <td>Max Experiments</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.max_experiments ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    }
+                    @if (anyIsDefined(systems, 'max_shots')) {
+                    <tr>
+                        <td>Max Shots</td>
+                        @for (system of systems; track $index) {
+                        <td>
+                            {{ system.max_shots ?? 'N/A' }}
+                        </td>
+                        }
+                    </tr>
+                    }
+                    <tr>
+                        <td>Last Checked</td>
+                        @for (system of systems; track $index) {
+                        <td>{{ system.last_checked | date: 'dd/MM/yy, HH:mm' }}</td>
+                        }
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    `,
+    styles: [`
+        .provider-logo {
+            width: 30px;
+            height: auto;
+            margin-right: 10px;
+        }
+    `]
+})
+export class SystemPropsComponent {
+    @Input() systems: System[];
+
+    constructor(private modalService: NgbModal) { }
+
+    openModal(system: System) {
+        const modalRef = this.modalService.open(NgbdModalContent);
+        modalRef.componentInstance.characterization = system.characterization;
+    }
+
+    anyIsDefined(systems: System[], field: string): boolean {
+        return systems.some(sys => sys[field] !== undefined);
+    }
+
+    normalizeName(name: string): string {
+        return name.replace(/ /g, '-').replace(/\b\w/g, l => l.toUpperCase())
+    }
+
+}
+
+
+@Component({
+    selector: 'ngbd-modal-content',
+    standalone: true,
+    template: `
+    <div class="modal-header">
+        <h4 class="modal-title">
+            Characterization
+            <a style="cursor: pointer;" (click)="openModal()">
+                <i class="fa-regular fa-circle-question" style="font-size: 16px;"
+                placement="bottom" ngbTooltip="Click for info about characterization"></i>
+            </a>
+        </h4>
+        <button type="button" class="btn-close" aria-label="Close" (click)="activeModal.dismiss('Cross click')"></button>
+    </div>
+    <div class="modal-body">
+        <pre>{{ characterization | json }}</pre>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" (click)="activeModal.close('Close click')">Close</button>
+    </div>
+    `,
+    imports: [JsonPipe, NgbTooltipModule]
+})
+export class NgbdModalContent {
+    activeModal = inject(NgbActiveModal);
+    @Input() characterization: string;
+
+    constructor(private modalService: NgbModal) { }
+
+    openModal() {
+        this.modalService.open(NgbdModalExplaination, { centered: true });
+    }
+}
+
+@Component({
+    selector: 'ngbd-modal-explaination',
+    standalone: true,
+    template: `
+    <div class="modal-header">
+        <h4 class="modal-title">
+            Characterization explaination
+        </h4>
+        <button type="button" class="btn-close" aria-label="Close" (click)="activeModal.dismiss('Cross click')"></button>
+    </div>
+    <div class="modal-body">
+        <p><strong>date:</strong> Time of the measurement, from Unix epoch in seconds.</p>
+        <p><strong>qubits:</strong> The number of qubits available.</p>
+        <p><strong>fidelity:</strong> Fidelity for single-qubit ('1q') and two-qubit ('2q') gates, and State Preparation and Measurement ('spam') operations. Currently provides only mean fidelity; additional statistical data will be added in the future.</p>
+        <p><strong>timing:</strong> Time, in seconds, of various system properties: 't1' time, 't2' time, '1q' gate time, '2q' gate time, 'readout' time, and qubit 'reset' time.</p>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" (click)="activeModal.close('Close click')">Close</button>
+    </div>
+    `,
+    imports: []
+})
+export class NgbdModalExplaination {
+    activeModal = inject(NgbActiveModal);
+}
+
