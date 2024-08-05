fb9f7bae4bb2ca698169ae7f9e81f5838ad6ba34
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-08-05 23:02:40 +0200

    feat(frontend): new and improved home page
    - Still in development

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,10 +1,76 @@
-﻿import { Component } from '@angular/core';
-
-import { AccountService } from '@app/_services';
+﻿import { Component, OnInit } from '@angular/core';
+import { HelperService } from '@app/_services/helper.service';
 
 @Component({ templateUrl: 'home.component.html' })
-export class HomeComponent {
-    account = this.accountService.accountValue;
+export class HomeComponent implements OnInit {
+    highestQubits?: number;
+    lowestQueue?: string;
 
-    constructor(private accountService: AccountService) { }
+    nProviders? = 0;
+    nSystems? = 0;
+
+    constructor(private helperService: HelperService) { }
+
+    ngOnInit() {
+        this.helperService.getAggregattion('backends', [
+            { $match: { status: { $in: ["online", "running", "available"] } } },
+            { $group: { _id: "null", maxQubits: { $max: "$qubits" } } }
+        ]).subscribe((response) => {
+            this.highestQubits = response[0]['maxQubits'];
+        });
+        this.helperService.getAggregattion('backends', [
+            {
+                $match:
+                {
+                    status: { $in: ["online", "running", "available"] },
+                    "queue.type": "avg_time"
+                }
+            },
+            { $group: { _id: "null", minQueue: { $min: { $toInt: "$queue.value" } } } }
+        ]).subscribe((response) => {
+            this.lowestQueue = this.convertFromMs(response[0]['minQueue']);
+        });
+
+        this.helperService.countDocuments('providers').subscribe((response) => {
+            this.nProviders = response['count'];
+        });
+        this.helperService.countDocuments('backends').subscribe((response) => {
+            this.nSystems = response['count'];
+        });
+    }
+
+    // Convierte tiempo en milisegundos a tiempo de espera con el formato de IonQ
+    convertFromMs(millisecStr: string | undefined): string {
+        if (millisecStr === undefined) {
+            return "N/A";
+        }
+        let millisec = parseInt(millisecStr);
+
+        // Convertir milisegundos a segundos
+        let seconds = Math.floor(millisec / 1000);
+
+        // Calcular días, horas y minutos
+        const mins = Math.floor(seconds / 60);
+        seconds = seconds % 60;
+        const hours = Math.floor(mins / 60);
+        const minutes = mins % 60;
+        const days = Math.floor(hours / 24);
+        const remainingHours = hours % 24;
+
+        let avgTimeInQueue = "";
+
+        if (days > 30) {
+            avgTimeInQueue = "> 1month";
+        } else if (days > 1) {
+            avgTimeInQueue = `${days}d ${remainingHours}hrs ${minutes}min`;
+        } else if (remainingHours > 1) {
+            avgTimeInQueue = `${remainingHours}hrs ${minutes}min`;
+        } else if (minutes > 1) {
+            avgTimeInQueue = `${minutes}min`;
+        } else {
+            avgTimeInQueue = "< 1min";
+        }
+
+        return avgTimeInQueue;
+    }
 }
