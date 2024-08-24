331adb5e3cd2048d29df71c301430b9378319b11
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-08-24 21:44:05 +0200

    refactor(frontend): minor changes
    - Plus modify docker environment
    - Plus modify angular.json

--- frontend/src/environments/environment.docker.ts
+++ frontend/src/environments/environment.docker.ts
@@ -3,5 +3,5 @@ export const environment = {
     // Como el código se ejecuta en el navegador, el contenedor con el backend no es visible
     // puesto que no se resuelve el nombre "backend", debería añadirse a la tabla de resolución de nombres
     // Solución sencilla: vincular el puerto 8000 del host al 80 del contenedor del backend
-    apiUrl: 'http://localhost:8000'
+    apiUrl: 'http://backend'
 };
