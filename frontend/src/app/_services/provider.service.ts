832f27bcfad3428999150d1256dd0edc23d1343b
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-08-29 03:51:01 +0200

    feat(frontend): new enpoints in provider service

--- frontend/src/app/_services/provider.service.ts
+++ frontend/src/app/_services/provider.service.ts
@@ -6,29 +6,29 @@ import { environment } from '@environments/environment';
 
 const baseUrl = `${environment.apiUrl}/providers`;
 
-const providersProjection = {
-  projection: {
-    name: 1,
-    description: 1,
-    website: 1,
-    from_third_party: 1,
-    backend_request: 1,
-    third_party: 1,
-    backends_ids: 1
-  }
-};
-
 
 @Injectable({ providedIn: 'root' })
 export class ProviderService {
 
   constructor(private http: HttpClient) { }
 
-  getAll() {
-    return this.http.post<Provider[]>(baseUrl, { providersProjection });
+  getAll(projection: any = null) {
+    return this.http.post<Provider[]>(`${baseUrl}/custom-query`, { ...projection });
   }
 
   getByPid(pid: string) {
     return this.http.get<Provider>(`${baseUrl}/${pid}`);
+  }
+
+  create(params: any) {
+    return this.http.post(baseUrl, params);
+  }
+
+  update(pid: string, params: any) {
+    return this.http.put(`${baseUrl}/${pid}`, params);
+  }
+
+  deleteByPid(pid: string) {
+    return this.http.delete(`${baseUrl}/${pid}`);
   }
 }
