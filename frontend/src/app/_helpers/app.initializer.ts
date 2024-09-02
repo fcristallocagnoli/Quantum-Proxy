bf2206a731baab4332758afa5fdc1e6145bfe040
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-09-02 02:34:36 +0200

    test(frontend): test new feature

--- frontend/src/app/_helpers/app.initializer.ts
+++ frontend/src/app/_helpers/app.initializer.ts
@@ -1,11 +1,19 @@
-import { catchError, of } from 'rxjs';
+import { catchError, of, timeout } from 'rxjs';
 
-import { AccountService } from '@app/_services';
+import { AccountService, AlertService } from '@app/_services';
 
-export function appInitializer(accountService: AccountService) {
+export function appInitializer(accountService: AccountService, alertService: AlertService) {
   return () => accountService.refreshToken()
-    .pipe(
+    .pipe(timeout(5000)).pipe(
       // catch error to start app on success or failure
-      catchError(() => of())
+      catchError((error) => {
+        alertService.warn(`
+          <h4>Backend server is starting up...</h4>
+          <p>It may take a few minutes for the backend server to start up and for the app to become fully functional.</p>
+          <p>Please wait for a while and then refresh the page.</p>
+        `, { autoClose: false, keepAfterRouteChange: true });
+        console.log('Error refreshing token', error);
+        return of();
+      })
     );
 }
