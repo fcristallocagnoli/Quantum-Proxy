599a7e7f54f349e052a2fe534cc2e5d092780d7c
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-09-02 03:16:13 +0200

    Revert "test(frontend): test new feature"
    This reverts commit bf2206a731baab4332758afa5fdc1e6145bfe040.

--- frontend/src/app/_helpers/app.initializer.ts
+++ frontend/src/app/_helpers/app.initializer.ts
@@ -1,19 +1,11 @@
-import { catchError, of, timeout } from 'rxjs';
+import { catchError, of } from 'rxjs';
 
-import { AccountService, AlertService } from '@app/_services';
+import { AccountService } from '@app/_services';
 
-export function appInitializer(accountService: AccountService, alertService: AlertService) {
+export function appInitializer(accountService: AccountService) {
   return () => accountService.refreshToken()
-    .pipe(timeout(5000)).pipe(
+    .pipe(
       // catch error to start app on success or failure
-      catchError((error) => {
-        alertService.warn(`
-          <h4>Backend server is starting up...</h4>
-          <p>It may take a few minutes for the backend server to start up and for the app to become fully functional.</p>
-          <p>Please wait for a while and then refresh the page.</p>
-        `, { autoClose: false, keepAfterRouteChange: true });
-        console.log('Error refreshing token', error);
-        return of();
-      })
+      catchError(() => of())
     );
 }
