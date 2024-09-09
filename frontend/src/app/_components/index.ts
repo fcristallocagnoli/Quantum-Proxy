3826210effb17d44ae20414b7cc630dabb26ca04
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-09-09 21:12:28 +0200

    chore(frontend): update components index

--- frontend/src/app/_components/index.ts
+++ frontend/src/app/_components/index.ts
@@ -1,2 +1,7 @@
 export * from './alert.component';
 export * from './confirmation.component';
+export * from './not-found.component';
+export * from './properties.component';
+export * from './theme.component';
+export * from './update-secrets.component';
+
