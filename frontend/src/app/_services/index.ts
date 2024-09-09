d258a88f6f24e9607bdc11ea637e6ae86ead6366
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2024-09-09 21:13:48 +0200

    chore(frontend): update services index

--- frontend/src/app/_services/index.ts
+++ frontend/src/app/_services/index.ts
@@ -1,4 +1,6 @@
 export * from './account.service';
 export * from './alert.service';
+export * from './helper.service';
+export * from './job.service';
 export * from './provider.service';
 export * from './system.service';
